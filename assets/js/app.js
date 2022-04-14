4f25ef9410be95527b1bede898162c9af65f753c
Muskandew07 <dewangan7muskan@gmail.com>
2022-04-14 18:34:19 +0530

    datatypes & operators

---
+++ assets/js/app.js
@@ -0,0 +1,84 @@
+/*
+// number data type
+let age = 25
+// string
+let brand = 'happy'
+// boolean value
+let isTrue = true
+console.log(isTrue)
+*/
+// let age = null 
+
+/* let age= 10
+
+console.log(typeof 10)
+console.log(typeof 19.131213123)
+console.log(typeof -123)
+console.log(typeof 'c')
+console.log(typeof 'String')
+console.log(typeof true)
+console.log(typeof null)
+console.log(typeof undefined)
+*/
+
+// assignmnt operators
+/* let age = 10
+
+
+console.log(++age)
+console.log(age)
+*/
+
+// comparison operator
+/*
+let a = 10
+let b = 15
+console.log(a>b)
+console.log(a<b)
+console.log(a>=b)
+console.log(a<=b)
+*/
+
+/* let a = 10
+let b = '10'
+console.log( a == b)
+console.log(a === b)
+*/
+/*
+let a = 10
+let b = 20
+console.log(a + b)
+console.log(a - b)
+console.log(a * b)
+console.log(a ** b)
+console.log(a % b)
+console.log(a / b)
+*/
+/*
+let a = 10
+let b = '10. 12345'
+console.log( a + Number(b))
+*/
+
+// logical operators
+console.log('true && true', true && true)
+console.log('true && false', true && false)
+console.log('false && true', false && true)
+console.log('false && false', false && false)
+
+console.log('true || true', true || true)
+console.log('true || false', true || false)
+console.log('false || true', false || true)
+console.log('false || false', false || false)
+
+
+
+
+
+ 
+/*
+const age = 80
+age = 18
+console.log(age)
+*/
+
