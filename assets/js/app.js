e98f01aa81195e206a32e68e75d7df97381b1a19
Muskandew07 <dewangan7muskan@gmail.com>
2022-04-16 00:03:54 +0530

    Day-17

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,25 +1,56 @@
-//  function to add value
 /*
-function add(a=0,b=0) {
-    const c = a + b
-    return c
+// JSON
+// literal notation to create an object
+const car = {
+    age : 18,
+    brand : "Mercedes",
+   
+    getBrand: function() { console.log(this.brand)}
+}
+// console.log(temp.age)
+// console.log(temp['brand'])
+
+
+// 
+car.getBrand()
+*/
+/*
+console.log(window.innerWidth)
+// window.alert("this is alert")
+console.log(typeof console)
+console.warn("i'm console.log" )
+console.error("i'm console.log" )
+
+*/
+// let arr = [1,2,3,4,5, "hi","john", "sheldon"]
+let arr=[1,2,3]
+
+// const callback = function (e,i) {
+   
+//     console.log(e,i)
+// }
+// arr.forEach((e,i) => console.log(e,i))
+/*
+for (let i=0; i<=arr.length; i++) {
+console.log(arr[i])
 }
 
-const sum = add(10,23)
-console.log(sum)
-console.log(add(10,23)) 
+let j =0
+while( j<arr.length) {
+    console.log(arr[j])
+    j++
+}
+*/ 
+/*
+const pushedElement = arr.push('hi')
+console.log(pushedElement)
+console.log(arr)
 */
 
+const callback = (ele,i) => { 
+    return ele = 10
 
-
-
-let greet2 = function(name) {
-    return "Hi" + name;
 }
-
-let greet1 = (name) => 'hi'
-
-
-console.log(greet1('john'))
-console.log(greet2('john'))
-
+const new_arr = arr.map(callback)
+console.log(new_arr)
+console.log(arr)
