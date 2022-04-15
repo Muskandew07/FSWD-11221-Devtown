e84c32106183503265cf95463f521ae750b46866
Muskandew07 <dewangan7muskan@gmail.com>
2022-04-15 11:50:03 +0530

    function

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,84 +1,25 @@
+//  function to add value
 /*
-// number data type
-let age = 25
-// string
-let brand = 'happy'
-// boolean value
-let isTrue = true
-console.log(isTrue)
-*/
-// let age = null 
+function add(a=0,b=0) {
+    const c = a + b
+    return c
+}
 
-/* let age= 10
-
-console.log(typeof 10)
-console.log(typeof 19.131213123)
-console.log(typeof -123)
-console.log(typeof 'c')
-console.log(typeof 'String')
-console.log(typeof true)
-console.log(typeof null)
-console.log(typeof undefined)
+const sum = add(10,23)
+console.log(sum)
+console.log(add(10,23)) 
 */
 
-// assignmnt operators
-/* let age = 10
-
-
-console.log(++age)
-console.log(age)
-*/
-
-// comparison operator
-/*
-let a = 10
-let b = 15
-console.log(a>b)
-console.log(a<b)
-console.log(a>=b)
-console.log(a<=b)
-*/
-
-/* let a = 10
-let b = '10'
-console.log( a == b)
-console.log(a === b)
-*/
-/*
-let a = 10
-let b = 20
-console.log(a + b)
-console.log(a - b)
-console.log(a * b)
-console.log(a ** b)
-console.log(a % b)
-console.log(a / b)
-*/
-/*
-let a = 10
-let b = '10. 12345'
-console.log( a + Number(b))
-*/
-
-// logical operators
-console.log('true && true', true && true)
-console.log('true && false', true && false)
-console.log('false && true', false && true)
-console.log('false && false', false && false)
-
-console.log('true || true', true || true)
-console.log('true || false', true || false)
-console.log('false || true', false || true)
-console.log('false || false', false || false)
 
 
 
+let greet2 = function(name) {
+    return "Hi" + name;
+}
+
+let greet1 = (name) => 'hi'
 
 
- 
-/*
-const age = 80
-age = 18
-console.log(age)
-*/
+console.log(greet1('john'))
+console.log(greet2('john'))
 
